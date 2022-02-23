4510684705864215e94fe0fdb362615071f51eae
Teddy Crane <teddy.crane@slalom.com>
2022-02-23 12:20:01 -0800

    update install script

--- vim/vim-plugins.sh
+++ vim/vim-plugins.sh
@@ -5,29 +5,57 @@ WORKDIR=$(pwd)
 if [[ -e "$HOME/.vimrc" ]]; then
   echo "Backing up existing vimrc"
   mv $HOME/.vimrc $HOME/.vimrc.bak
-  ln -s $WORKDIR/.vimrc $HOME/.vimrc
 fi
 
-echo "Installing pathogen"
-mkdir -p $HOME/.vim/{autoload,bundle}
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Linking .vimrc" 
+ln -s $WORKDIR/.vimrc $HOME/.vimrc
 
-cd $HOME/.vim/bundle
+echo "Installing pathogen"
+if [[ ! -e "$HOME/.vim/autoload/pathogen.vim" ]]; then
+  if [[ ! -d "$HOME/.vim/bundle" ]]; then
+    mkdir -p $HOME/.vim/{autoload,bundle}
+  fi
+  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+fi
+
+pushd $HOME/.vim/bundle
 echo "Installing vim utilities"
-git clone https://github.com/preservim/nerdtree.git
-git clone https://github.com/vim-airline/vim-airline
+
+if [[ ! -d "./nerdtree" ]]; then
+  echo "Installing NERDTree"
+  git clone https://github.com/preservim/nerdtree.git
+fi
+
+if [[ ! -d "./vim-airline" ]]; then
+  echo "Installing vim-airline"
+  git clone https://github.com/vim-airline/vim-airline
+fi
 
 echo "Installing vim development plugins"
-git clone https://github.com/dense-analysis/ale
-git clone https://github.com/airblade/vim-gitgutter
-git clone https://github.com/liuchengxu/vista.vim
-git clone https://github.com/leafgarland/typescript-vim
-git clone https://github.com/peitalin/vim-jsx-typescript
-git clone https://github.com/neoclide/coc.nvim
+if [[ ! -d "./ale" ]]; then
+  git clone https://github.com/dense-analysis/ale
+fi
+if [[ ! -d "./vim-gitgutter" ]]; then
+  git clone https://github.com/airblade/vim-gitgutter
+fi
+if [[ ! -d "./vista.vim" ]]; then
+  git clone https://github.com/liuchengxu/vista.vim
+fi
+if [[ ! -d "./typescript-vim" ]]; then
+  git clone https://github.com/leafgarland/typescript-vim
+fi
+if [[ ! -d  "./vim-jsx-typescript" ]]; then
+  git clone https://github.com/peitalin/vim-jsx-typescript
+fi
+if [[ ! -d "./coc.nvim" ]]; then
+  git clone https://github.com/neoclide/coc.nvim
+fi
 
 echo "Installing dependencies for coc.nvim"
-cd coc.nvim && yarn install
-cd
+pushd coc.nvim 
+yarn install
+popd
 echo "Done!"
+popd
 exit 0
 
